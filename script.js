1cbe1721a86343b01c188002756daaf9e31a27fb
Korni <kkupcin@gmail.com>
2021-11-17 19:10:02 +0200

    basic html and css

---
+++ script.js
@@ -0,0 +1,7 @@
+const billInputValue = document.querySelector("#bill-input").value;
+const customTipValue = document.querySelector("#custom-tip").value;
+const noOfPeople = document.querySelector("#no-of-people").value;
+const tipAmountList = document.querySelectorAll("[data-tip]");
+const tipTotalShown = document.querySelector(".tip-amount");
+const totalShown = document.querySelector(".total-amount");
+const resetBtn = document.querySelector(".reset-button");
