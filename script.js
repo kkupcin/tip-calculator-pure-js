31bc43fe3509857e84cab21c7d1dd239b4b3f60b
Korni <kkupcin@gmail.com>
2021-11-21 10:55:31 +0200

    added styling and basic logic

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-const billInputValue = document.querySelector("#bill-input").value;
-const customTipValue = document.querySelector("#custom-tip").value;
-const noOfPeople = document.querySelector("#no-of-people").value;
-const tipAmountList = document.querySelectorAll("[data-tip]");
-const tipTotalShown = document.querySelector(".tip-amount");
-const totalShown = document.querySelector(".total-amount");
-const resetBtn = document.querySelector(".reset-button");
+let billInput = document.querySelector("#bill-input");
+let customTip = document.querySelector("#custom-tip");
+let noOfPeople = document.querySelector("#no-of-people");
+let tipAmountList = document.querySelectorAll("[data-tip]");
+let tipTotalShown = document.querySelector(".tip-amount");
+let totalShown = document.querySelector(".total-amount");
+let resetBtn = document.querySelector(".reset-button");
+
+let tipAmount = 0;
+let billAmount = 0;
+
+billInput.addEventListener("input", (e) => {
+  console.log(tipAmount);
+  calculateTip(tipAmount || 0);
+  calculateTotal(tipAmount || 0);
+  calculatePerPerson(tipAmount, parseInt(e.target.value) || 0);
+});
+
+customTip.addEventListener("input", (e) => {
+  calculateTip(parseInt(e.target.value.trim()) || 0);
+  calculateTotal(parseInt(e.target.value.trim()) || 0);
+  calculatePerPerson(tipAmount, billAmount);
+});
+
+noOfPeople.addEventListener("input", (e) => {
+  calculateTip(tipAmount || 0);
+  calculateTotal(tipAmount || 0);
+  calculatePerPerson(tipAmount, billAmount);
+});
+
+const calculateTip = (tip) => {
+  tipAmount = parseInt(billInput.value) * (parseInt(tip) / 100);
+};
+
+const calculateTotal = (tip) => {
+  billAmount = parseInt(tip) + parseInt(billInput.value);
+};
+
+calculatePerPerson = (tip, bill) => {
+  totalShown.innerText = `$${
+    parseFloat(billAmount).toFixed(2) /
+    (parseInt(noOfPeople.value) || 1).toFixed(2)
+  }`;
+  tipTotalShown.innerText = `$${
+    parseFloat(tipAmount).toFixed(2) /
+    (parseInt(noOfPeople.value) || 1).toFixed(2)
+  }`;
+};
